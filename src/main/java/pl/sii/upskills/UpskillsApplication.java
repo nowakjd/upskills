4b27dff58421b41b51b2c556b8a323cb72682930
mmoleda1 <mmoleda1@sii.pl>
2022-01-27 17:00:53 +0100

    setup new app

---
+++ src/main/java/pl/sii/upskills/UpskillsApplication.java
@@ -0,0 +1,13 @@
+package pl.sii.upskills;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UpskillsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UpskillsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/sii/upskills/UpskillsApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.sii.upskills;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UpskillsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
