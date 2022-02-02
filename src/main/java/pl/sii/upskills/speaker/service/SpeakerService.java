6cdef079483ddb849ad2e4dba81a6562bd3638c6
mmoleda1 <mmoleda1@sii.pl>
2022-02-02 22:44:27 +0100

    Mapper was changed to functional interface

--- src/main/java/pl/sii/upskills/speaker/service/SpeakerService.java
+++ src/main/java/pl/sii/upskills/speaker/service/SpeakerService.java
@@ -4,13 +4,15 @@ import org.springframework.stereotype.Service;
 import pl.sii.upskills.speaker.persistence.Speaker;
 import pl.sii.upskills.speaker.persistence.SpeakerRepository;
 
+import java.util.function.Function;
+
 @Service
 public class SpeakerService {
     private final SpeakerRepository speakerRepository;
     private final SpeakerInputValidator speakerInputValidator;
-    private final Mapper mapper;
+    private final Function<SpeakerInput, Speaker> mapper;
 
-    public SpeakerService(SpeakerRepository speakerRepository, SpeakerInputValidator speakerInputValidator, Mapper mapper) {
+    public SpeakerService(SpeakerRepository speakerRepository, SpeakerInputValidator speakerInputValidator, Function<SpeakerInput, Speaker> mapper) {
         this.speakerRepository = speakerRepository;
         this.speakerInputValidator =speakerInputValidator;
         this.mapper = mapper;
