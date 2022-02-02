64ef5e2b85f97c322200ff08bd3ed06be68fffae
Tomasz Dziędziewicki <tdziedziewicki@sii.pl>
2022-02-02 16:52:38 +0100

    SpeakerInputValidatorTest refactor

--- src/test/java/pl/sii/upskills/speaker/service/SpeakerInputValidatorTest.java
+++ src/test/java/pl/sii/upskills/speaker/service/SpeakerInputValidatorTest.java
@@ -4,13 +4,17 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.function.Executable;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.NullSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import java.util.stream.Stream;
+
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
 
 class SpeakerInputValidatorTest {
 
@@ -31,27 +35,42 @@ class SpeakerInputValidatorTest {
     @Test
     @DisplayName("Validation should pass without email")
     void noEmail() {
+        // given
         SpeakerInputValidator underTest = new SpeakerInputValidator();
         SpeakerInput input = new SpeakerInput("John", "Doe", "123456789", "", "My bio");
+
+        // when
         boolean validate = underTest.validate(input);
+
+        // then
         assertTrue(validate);
     }
 
     @Test
     @DisplayName("Validation should pass without phone number")
     void noPhone() {
+        // given
         SpeakerInputValidator underTest = new SpeakerInputValidator();
         SpeakerInput input = new SpeakerInput("John", "Doe", "", "john@email.com", "My bio");
+
+        // when
         boolean validate = underTest.validate(input);
+
+        // then
         assertTrue(validate);
     }
 
     @Test
     @DisplayName("Validation should pass without bio")
     void noBio() {
+        // given
         SpeakerInputValidator underTest = new SpeakerInputValidator();
         SpeakerInput input = new SpeakerInput("John", "Doe", "123456789", "john@email.com", "");
+
+        // when
         boolean validate = underTest.validate(input);
+
+        // then
         assertTrue(validate);
     }
 
@@ -74,34 +93,41 @@ class SpeakerInputValidatorTest {
                 .allMatch(e -> e.contains("First name is required"));
     }
 
-    @Test
+    @ParameterizedTest
+    @NullSource
+    @ValueSource(strings = {" ", "    ", "\t"})
     @DisplayName("Validation should throw exception when last name is empty")
-    void lastNameEmpty() {
+    void lastNameEmpty(String lastName) {
+        // given
         SpeakerInputValidator underTest = new SpeakerInputValidator();
-        SpeakerInput input = new SpeakerInput("John", "", "123456789", "john@email.com", "My bio");
-        SpeakerValidationException exception = assertThrows(SpeakerValidationException.class, () -> {
-            boolean validate = underTest.validate(input);
-        });
-        String expectedMessage = "Last name is required";
-        if (exception.getErrors().size() == 1) {
-            String actualMessage = exception.getErrors().get(0);
-            assertTrue(actualMessage.contains(expectedMessage));
-        } else assertEquals(1, exception.getErrors().size());
+        SpeakerInput input = new SpeakerInput("John", lastName, "123456789", "john@email.com", "My bio");
+
+        // when
+        Executable underTestLambda = () -> underTest.validate(input);
+
+        // then
+        SpeakerValidationException exception = assertThrows(SpeakerValidationException.class, underTestLambda);
+        assertThat(exception.getErrors())
+                .hasSize(1)
+                .allMatch(e->e.contentEquals("Last name is required"));
     }
 
-    @Test
+    @ParameterizedTest
+    @MethodSource("twoArgsProvider")
     @DisplayName("Validation should throw exception when email or phone number is empty")
-    void emailAndPhoneEmpty() {
+    void emailAndPhoneEmpty(String email, String phoneNumber) {
+        // given
         SpeakerInputValidator underTest = new SpeakerInputValidator();
-        SpeakerInput input = new SpeakerInput("John", "Doe", "", "", "My bio");
-        SpeakerValidationException exception = assertThrows(SpeakerValidationException.class, () -> {
-            boolean validate = underTest.validate(input);
-        });
-        String expectedMessage = "Email or phone number is required";
-        if (exception.getErrors().size() == 1) {
-            String actualMessage = exception.getErrors().get(0);
-            assertTrue(actualMessage.contains(expectedMessage));
-        } else assertEquals(1, exception.getErrors().size());
+        SpeakerInput input = new SpeakerInput("John", "Doe", email, phoneNumber, "My bio");
+
+        // when
+        Executable underTestLambda = () -> underTest.validate(input);
+
+        // then
+        SpeakerValidationException exception = assertThrows(SpeakerValidationException.class, underTestLambda);
+        assertThat(exception.getErrors())
+                .hasSize(1)
+                .allMatch(e->e.contains("Email or phone number is required"));
     }
 
     @Test
@@ -121,72 +147,84 @@ class SpeakerInputValidatorTest {
                 .anyMatch(e -> e.contains("First name is required"))
                 .anyMatch(e -> e.contains("Last name is required"))
                 .anyMatch(e -> e.contains("Email or phone number is required"));
+   }
 
-//        String expectedFirstNameMessage = "First name is required";
-//        String expectedLastNameMessage = "Last name is required";
-//        String expectedPhoneOrEmailMessage = "Email or phone number is required";
-//        if (exception.getErrors().size() == 3) {
-//            String actualFirstNameMessage = exception.getErrors().get(0);
-//            assertTrue(expectedFirstNameMessage.contains(actualFirstNameMessage));
-//            String actualLastNameMessage = exception.getErrors().get(1);
-//            assertTrue(expectedLastNameMessage.contains(actualLastNameMessage));
-//            String actualPhoneOrEmailMessage = exception.getErrors().get(2);
-//            assertTrue(expectedPhoneOrEmailMessage.contains(actualPhoneOrEmailMessage));
-//        } else assertEquals(3, exception.getErrors().size());
-    }
-
-    @Test
+    @ParameterizedTest
+    @MethodSource("twoArgsProvider")
     @DisplayName("Validation should throw exception when first and last name are empty")
-    void firstNameAndLastNameAreEmpty() {
+    void firstNameAndLastNameAreEmpty(String firstName, String lastName) {
+        // given
         SpeakerInputValidator underTest = new SpeakerInputValidator();
-        SpeakerInput input = new SpeakerInput("", "", "123456789", "john@email.com", "My bio");
-        SpeakerValidationException exception = assertThrows(SpeakerValidationException.class, () -> {
-            boolean validate = underTest.validate(input);
-        });
-        String expectedFirstNameMessage = "First name is required";
-        String expectedLastNameMessage = "Last name is required";
-        if (exception.getErrors().size() == 2) {
-            String actualFirstNameMessage = exception.getErrors().get(0);
-            assertTrue(expectedFirstNameMessage.contains(actualFirstNameMessage));
-            String actualLastNameMessage = exception.getErrors().get(1);
-            assertTrue(expectedLastNameMessage.contains(actualLastNameMessage));
-        } else assertEquals(2, exception.getErrors().size());
+        SpeakerInput input = new SpeakerInput(firstName, lastName, "123456789", "john@email.com", "My bio");
+
+        //when
+        Executable underTestLambda = () -> underTest.validate(input);
+
+        //then
+        SpeakerValidationException exception = assertThrows(SpeakerValidationException.class, underTestLambda);
+        assertThat(exception.getErrors())
+                .hasSize(2)
+                .anyMatch(e->e.contains("First name is required"))
+                .anyMatch(e->e.contains("Last name is required"));
     }
 
-    @Test
+    @ParameterizedTest
+    @MethodSource("threeArgsProvider")
     @DisplayName("Validation should throw exception when first name and email and phone number are empty")
-    void firstNameAndPhoneNumberAndEmailEmpty() {
+    void firstNameAndPhoneNumberAndEmailEmpty(String firstName, String phoneNumber, String email) {
+        // given
         SpeakerInputValidator underTest = new SpeakerInputValidator();
-        SpeakerInput input = new SpeakerInput("", "Doe", "", "", "");
-        SpeakerValidationException exception = assertThrows(SpeakerValidationException.class, () -> {
-            boolean validate = underTest.validate(input);
-        });
-        String expectedFirstNameMessage = "First name is required";
-        String expectedPhoneOrEmailMessage = "Email or phone number is required";
-        if (exception.getErrors().size() == 2) {
-            String actualFirstNameMessage = exception.getErrors().get(0);
-            assertTrue(expectedFirstNameMessage.contains(actualFirstNameMessage));
-            String actualPhoneOrEmailMessage = exception.getErrors().get(1);
-            assertTrue(expectedPhoneOrEmailMessage.contains(actualPhoneOrEmailMessage));
-        } else assertEquals(2, exception.getErrors().size());
+        SpeakerInput input = new SpeakerInput(firstName, "Doe", phoneNumber, email, "");
+
+        //when
+        Executable underTestLambda = () -> underTest.validate(input);
+
+        //then
+        SpeakerValidationException exception = assertThrows(SpeakerValidationException.class, underTestLambda);
+        assertThat(exception.getErrors())
+                .hasSize(2)
+                .anyMatch(e->e.contains("First name is required"))
+                .anyMatch(e->e.contains("Email or phone number is required"));
+   }
+
+    @ParameterizedTest
+    @MethodSource("threeArgsProvider")
+    @DisplayName("Validation should throw exception when last name and email and phone number are empty")
+    void lastNameAndPhoneNumberAndEmailEmpty(String lastName, String phoneNumber, String email) {
+        // given
+        SpeakerInputValidator underTest = new SpeakerInputValidator();
+        SpeakerInput input = new SpeakerInput("John", lastName, phoneNumber, email, "");
+
+        //when
+        Executable underTestLambda = () -> underTest.validate(input);
+
+        //then
+        SpeakerValidationException exception = assertThrows(SpeakerValidationException.class, underTestLambda);
+        assertThat(exception.getErrors())
+                .hasSize(2)
+                .anyMatch(e->e.contains("Last name is required"))
+                .anyMatch(e->e.contains("Email or phone number is required"));
     }
 
-    @Test
-    @DisplayName("Validation should throw exception when last name and email and phone number are empty")
-    void lastNameAndPhoneNumberAndEmailEmpty() {
-        SpeakerInputValidator underTest = new SpeakerInputValidator();
-        SpeakerInput input = new SpeakerInput("John", "", "", "", "");
-        SpeakerValidationException exception = assertThrows(SpeakerValidationException.class, () -> {
-            boolean validate = underTest.validate(input);
-        });
-        String expectedLastNameMessage = "Last name is required";
-        String expectedPhoneOrEmailMessage = "Email or phone number is required";
-        if (exception.getErrors().size() == 2) {
-            String actualLastNameMessage = exception.getErrors().get(0);
-            assertTrue(expectedLastNameMessage.contains(actualLastNameMessage));
-            String actualPhoneOrEmailMessage = exception.getErrors().get(1);
-            assertTrue(expectedPhoneOrEmailMessage.contains(actualPhoneOrEmailMessage));
-        } else assertEquals(2, exception.getErrors().size());
+    private static Stream<Arguments> twoArgsProvider() {
+        return Stream.of(
+                arguments(" ", "    "),
+                arguments(" ", "\t"),
+                arguments("    ", " "),
+                arguments("\t", ""),
+                arguments(null, " ")
+
+        );
+    }
+    private static Stream<Arguments> threeArgsProvider() {
+        return Stream.of(
+                arguments(" ", "    ", "\t"),
+                arguments(" ", "\t", null),
+                arguments("    ", " ", ""),
+                arguments("\t", "", "   "),
+                arguments(null, " ", null)
+
+        );
     }
 
 }
