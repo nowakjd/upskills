052a8dc7c55157e36a8a052d1f8497dc01991758
Kamil Nowacki <knowacki1@sii.pl>
2022-02-04 14:18:01 +0100

    Set in .env your username and password and set up environment variables

--- src/test/java/pl/sii/upskills/UpskillsApplicationTests.java
+++ src/test/java/pl/sii/upskills/UpskillsApplicationTests.java
@@ -1,13 +1,27 @@
 package pl.sii.upskills;
 
+import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class UpskillsApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+// Speaker speaker = new Speaker();
+
+// @Test
+// void contextLoads() {
+// }
+
+// @Test
+// void speakerActive(){
+// 	speaker.activate();
+// }
+
+// @Test
+// void speakerDeactivated(){
+// 	speaker.deactivate();
+// 	Assert.assertTrue("jest poprawny", true);
+// }
 
 }
