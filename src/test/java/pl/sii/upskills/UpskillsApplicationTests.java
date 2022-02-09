aaf7c1804d2cb6aa84a02aced279e508c713391f
Kamil Nowacki <knowacki1@sii.pl>
2022-02-09 12:24:51 +0100

    Setting the default values to application.properties and docker-compose

--- src/test/java/pl/sii/upskills/UpskillsApplicationTests.java
+++ src/test/java/pl/sii/upskills/UpskillsApplicationTests.java
@@ -7,22 +7,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class UpskillsApplicationTests {
 
-// Speaker speaker = new Speaker();
-
     @Disabled
     @Test
     void contextLoads() {
     }
-
-// @Test
-// void speakerActive(){
-// 	speaker.activate();
-// }
-
-// @Test
-// void speakerDeactivated(){
-// 	speaker.deactivate();
-// 	Assert.assertTrue("jest poprawny", true);
-// }
 
 }
