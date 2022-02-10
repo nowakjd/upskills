ba15f37e40c1e2a899661405c543f3940aa92ae4
Kamil Nowacki <knowacki1@sii.pl>
2022-02-10 09:33:19 +0100

    Change properties, and disable test

--- src/test/java/pl/sii/upskills/UpskillsApplicationTests.java
+++ src/test/java/pl/sii/upskills/UpskillsApplicationTests.java
@@ -5,9 +5,9 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@Disabled
 class UpskillsApplicationTests {
 
-    @Disabled
     @Test
     void contextLoads() {
     }
