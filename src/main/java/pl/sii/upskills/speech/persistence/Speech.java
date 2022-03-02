4ce2309285263bfe1d58f413a86bf3eda6685f46
Tomasz Dziędziewicki <tdziedziewicki@sii.pl>
2022-03-02 12:01:16 +0100

    db fix

--- src/main/java/pl/sii/upskills/speech/persistence/Speech.java
+++ src/main/java/pl/sii/upskills/speech/persistence/Speech.java
@@ -13,7 +13,7 @@ import java.util.Set;
 public class Speech {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id", nullable = false)
     private Long id;
     @Column(nullable = false)
