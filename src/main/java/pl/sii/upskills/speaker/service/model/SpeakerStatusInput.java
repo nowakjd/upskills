7d075dcb8156a6376b377e2122b8ffc1a49a4386
Łukasz Matuła <lmatula@sii.pl>
2022-03-07 19:17:10 +0100

    add annotation @JasonCreator

--- src/main/java/pl/sii/upskills/speaker/service/model/SpeakerStatusInput.java
+++ src/main/java/pl/sii/upskills/speaker/service/model/SpeakerStatusInput.java
@@ -1,11 +1,13 @@
 package pl.sii.upskills.speaker.service.model;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
 import pl.sii.upskills.speaker.persistence.SpeakerStatus;
 
 public class SpeakerStatusInput {
 
     private final SpeakerStatus status;
 
+    @JsonCreator
     public SpeakerStatusInput(SpeakerStatus status) {
         this.status = status;
     }
