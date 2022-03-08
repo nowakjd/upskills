c7b9a38b3a30d9269f7649703a06f226d674c7d4
Tomasz Dziędziewicki <tdziedziewicki@sii.pl>
2022-03-08 14:34:16 +0100

    timeslottest refactor

--- src/test/java/pl/sii/upskills/conference/service/model/TimeSlotTest.java
+++ src/test/java/pl/sii/upskills/conference/service/model/TimeSlotTest.java
@@ -11,6 +11,12 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 class TimeSlotTest {
 
+    private static final LocalDateTime BASE_START_DATE = LocalDateTime.of(2022, 4, 22, 8,
+            5);
+    private static final LocalDateTime BASE_END_DATE = LocalDateTime.of(2022, 4, 22, 10,
+            5);
+
+
     @Test
     @DisplayName("Timeslots aren't colliding")
     void happyPath() {
@@ -132,43 +138,35 @@ class TimeSlotTest {
     }
 
     private TimeSlotVO createTestTimeslot() {
-        return new TimeSlotVO(LocalDateTime.of(2022, 4, 22, 8,
-                5), LocalDateTime.of(2022, 4, 22, 10, 5));
+        return new TimeSlotVO(BASE_START_DATE, BASE_END_DATE);
     }
 
     private TimeSlotVO createCollidingTimeslot() {
-        return new TimeSlotVO(LocalDateTime.of(2022, 4, 22, 9,
-                5), LocalDateTime.of(2022, 4, 22, 11, 5));
+        return new TimeSlotVO(BASE_START_DATE.plusHours(1), BASE_END_DATE.plusHours(1));
     }
 
     private TimeSlotVO createEarlierCollidingTimeslot() {
-        return new TimeSlotVO(LocalDateTime.of(2022, 4, 22, 7,
-                5), LocalDateTime.of(2022, 4, 22, 9, 5));
+        return new TimeSlotVO(BASE_START_DATE.minusHours(1), BASE_END_DATE.minusHours(1));
     }
 
     private TimeSlotVO createNonCollidingTimeslot() {
-        return new TimeSlotVO(LocalDateTime.of(2022, 4, 22, 12,
-                5), LocalDateTime.of(2022, 4, 22, 14, 5));
+        return new TimeSlotVO(BASE_START_DATE.plusHours(4), BASE_END_DATE.plusHours(4));
     }
 
     private TimeSlotVO createSameStartDateTimeslot() {
-        return new TimeSlotVO(LocalDateTime.of(2022, 4, 22, 8,
-                5), LocalDateTime.of(2022, 4, 22, 9, 5));
+        return new TimeSlotVO(BASE_START_DATE, BASE_END_DATE.minusHours(1));
     }
 
     private TimeSlotVO createSameEndDateTimeslot() {
-        return new TimeSlotVO(LocalDateTime.of(2022, 4, 22, 7,
-                5), LocalDateTime.of(2022, 4, 22, 10, 5));
+        return new TimeSlotVO(BASE_START_DATE.plusHours(1), BASE_END_DATE);
     }
 
     private TimeSlotVO createEndDateEqualToStartDate() {
-        return new TimeSlotVO(LocalDateTime.of(2022, 4, 22, 6,
-                5), LocalDateTime.of(2022, 4, 22, 8, 5));
+        return new TimeSlotVO(BASE_START_DATE.minusHours(2), BASE_START_DATE);
     }
 
     private TimeSlotVO createStartDateEqualToEndDate() {
-        return new TimeSlotVO(LocalDateTime.of(2022, 4, 22, 10,
-                5), LocalDateTime.of(2022, 4, 22, 12, 5));
+        return new TimeSlotVO(BASE_END_DATE, BASE_END_DATE.plusHours(2));
     }
 
 }
