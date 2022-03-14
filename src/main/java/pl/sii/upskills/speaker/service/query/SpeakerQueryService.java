b73e46a6cac782654baa8f86328f4ddafb18dd76
Jedrzej Nowak <jnowak3@sii.pl>
2022-03-14 14:03:28 +0100

    SpeakerQueryService

--- src/main/java/pl/sii/upskills/speaker/service/query/SpeakerQueryService.java
+++ src/main/java/pl/sii/upskills/speaker/service/query/SpeakerQueryService.java
@@ -5,16 +5,10 @@ import pl.sii.upskills.speaker.persistence.Speaker;
 import pl.sii.upskills.speaker.persistence.SpeakerRepository;
 import pl.sii.upskills.speaker.persistence.SpeakerStatus;
 import pl.sii.upskills.speaker.service.command.SpeakerBadRequestException;
-import pl.sii.upskills.speaker.service.command.SpeakerNotFoundException;
 import pl.sii.upskills.speaker.service.model.SpeakerOutput;
 
-import javax.transaction.Transactional;
 import java.util.*;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Set;
 import java.util.function.Function;
-import java.util.stream.Collectors;
 
 @Service
 public class SpeakerQueryService {
@@ -60,19 +54,4 @@ public class SpeakerQueryService {
         return speakerRepository.findByIdIn(ids);
     }
 
-
-    @Transactional
-    public Set<Speaker> getSpeakersIds(Set<Long> ids) {
-        return Optional.of(ids)
-                .orElse(Collections.emptySet())
-                .stream()
-                .map(this::getSpeaker)
-                .collect(Collectors.toSet());
-    }
-
-    private Speaker getSpeaker(Long i) {
-        return speakerRepository
-                .findById(i)
-                .orElseThrow(() -> new SpeakerNotFoundException(i));
-    }
 }
