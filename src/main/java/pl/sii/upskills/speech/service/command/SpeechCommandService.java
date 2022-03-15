48fc2a19288b17fe931f335673e15ff598bd2831
Tomasz Dziędziewicki <tdziedziewicki@sii.pl>
2022-03-15 17:06:33 +0100

    database retrieval refactor

--- src/main/java/pl/sii/upskills/speech/service/command/SpeechCommandService.java
+++ src/main/java/pl/sii/upskills/speech/service/command/SpeechCommandService.java
@@ -53,8 +53,8 @@ public class SpeechCommandService {
     @Transactional
     public SpeechOutput addSpeakers(UUID conferenceId, Long id, SpeechSpeakersInput speechSpeakersInput) {
         Speech speech = getSpeech(id);
-        Conference conference = getConference(conferenceId);
-        speechConferenceValidator.validate(conference, speech);
+        Conference conference = speech.getConference();
+        speechConferenceValidator.validate(conference, conferenceId);
         Set<Speaker> speakerSet = speakerQueryService.getSpeakersByIds(speechSpeakersInput.getIds());
         speech.setSpeakerSet(speakerSet);
         speakerValidator.validateSpeakers(speech, speechSpeakersInput);
@@ -63,19 +63,20 @@ public class SpeechCommandService {
 
     @Transactional
     public SpeechOutput updateSpeech(UUID conferenceId, Long id, SpeechInput speechInput) {
-        Conference conference = getConference(conferenceId);
         Speech speech = getSpeech(id);
-        speechConferenceValidator.validate(conference, speech);
+        Conference conference = speech.getConference();
+        speechConferenceValidator.validate(conference, conferenceId);
         speechInputValidator.validate(speechInput, conference);
+        speechMapper.apply(speech, speechInput);
         speechRepository.save(speech);
         return speechOutputMapper.apply(speech);
     }
 
     @Transactional
     public SpeechOutput addSpeaker(UUID conferenceId, Long speechId, Long id) {
-        Conference conference = getConference(conferenceId);
         Speech speech = getSpeech(speechId);
-        speechConferenceValidator.validate(conference, speech);
+        Conference conference = speech.getConference();
+        speechConferenceValidator.validate(conference, conferenceId);
         Speaker speakerToAdd = speakerQueryService.getSpeakerById(id);
         if (!speech.getSpeakerSet().contains(speakerToAdd)) {
             speech.addSpeaker(speakerToAdd);
@@ -87,9 +88,9 @@ public class SpeechCommandService {
 
     @Transactional
     public SpeechOutput deleteSpeaker(UUID conferenceId, Long speechId, Long id) {
-        Conference conference = getConference(conferenceId);
         Speech speech = getSpeech(speechId);
-        speechConferenceValidator.validate(conference, speech);
+        Conference conference = speech.getConference();
+        speechConferenceValidator.validate(conference, conferenceId);
         Speaker speakerToRemove = speakerQueryService.getSpeakerById(id);
         speech.removeSpeaker(speakerToRemove);
         speechRepository.save(speech);
